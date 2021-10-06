3cf81874133c94423da7fe41fc3c92c7dcc8d4b1
DigoRM <82246925+DigoRM@users.noreply.github.com>
2021-10-06 15:48:57 -0300

    Update Estornos_CX.py

--- Estornos_CX.py
+++ Estornos_CX.py
@@ -93,9 +93,10 @@ except Exception as e:
 	print(e)
 
 # processo para poder efetuar operações matematicas com o horario
-# df2['Valor do Estorno'] = df2['Valor do Estorno'].astype(str)
-df2['Valor do Estorno'] = df2['Valor do Estorno'].str[3:]
+df2['Valor do Estorno'] = df2['Valor do Estorno'].astype(str)
 df2['Valor do Estorno'] = df2['Valor do Estorno'].str.replace(r',', '.')
+df2['Valor do Estorno'] = df2['Valor do Estorno'].str[3:]
+
 df2
 df2['Valor do Estorno'] = df2['Valor do Estorno'].astype(float)
 
